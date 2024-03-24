2ba42d7807192b8cc987de6725afea18f144cd5a
T-Roberson <149193083+T-Roberson@users.noreply.github.com>
2024-03-24 15:10:40 -0400

    Completed Question 1

---
+++ Python_Syntax.py
@@ -0,0 +1,21 @@
+# Python Syntax Assignment
+
+#1.1 Code Correction
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#1.2 Your Mood Today
+
+userAnswer = input("How are you feeling today?\n")
+if userAnswer == "happy":
+    print("That's great to hear!");
+elif userAnswer == "sad":
+    print("I hope your day gets better!")
+else:
+    print("... Oooookay then!")
+
